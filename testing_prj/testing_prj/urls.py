9b77dde4eac177fec2e0c47b055be4003a5c1452
danleyb2 <ndieksman@gmail.com>
2016-04-04 11:54:59 +0000

    add url for testing app

--- testing_prj/testing_prj/urls.py
+++ testing_prj/testing_prj/urls.py
@@ -2,10 +2,10 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 #from test_app import views
-from pa import views
+#from pa import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^test/',include('test_app.urls')),
-    url(r'^pa/',include('pa.urls')),
+    url(r'^pa_dev/',include('package_dev.urls')),
+    #url(r'^pa/',include('pa.urls')),
 ]
