d574b86d57780a47307b339412606ecf0586ea7a
danleyb2 <ndieksman@gmail.com>
2016-04-13 13:27:37 +0000

    now able to serve media files

--- testing_prj/testing_prj/urls.py
+++ testing_prj/testing_prj/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url,include
 from django.contrib import admin
 #from test_app import views
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^pa_dev/',include('package_dev.urls')),
     #url(r'^pa/',include('pa.urls')),
-]
+    url(r'^rog/',include('rog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
